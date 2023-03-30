47126fa0e69db92ef16943928b68894060e49e5c
jacob <jacob.7050@gmail.com>
2023-03-29 21:26:19 -0700

    post.js has been saved and updated

---
+++ models/post.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+const { format } = require('prettier');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    Post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    Image_path: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      image_path:varchar(255).NOTNULL,
+
+    },
+    caption: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    creation_date: {
+        date: Date,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+  }
+);
+
+module.exports = Post;
