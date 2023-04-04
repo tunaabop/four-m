2e5b7e26dc442ebe707bfea627e73a700a3bc27c
jacob <jacob.7050@gmail.com>
2023-04-03 19:15:31 -0700

    Git ignore added

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,3 @@
--- drop database if exists DB_NAME;
+drop database if exists fourm_db;
 create database fourm_db;
+
--- models/post.js
+++ models/post.js
@@ -17,22 +17,16 @@ Post.init(
     image_path: {
       type: DataTypes.STRING,
       allowNull: false,
-      image_path:varchar(255).NOTNULL,
 
     },
     caption: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-<<<<<<< HEAD
-    creation_date: {
-        date: Date,
-=======
     date_created: {
       type: DataTypes.DATE,
       allowNull: false,
       defaultValue: DataTypes.NOW,
->>>>>>> c1365ebcac449715783e508dfaf86c950a41a0f9
     },
     user_id: {
       type: DataTypes.INTEGER,
