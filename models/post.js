0212d83be403a23e8f85b41dd9c4af564a3dc2e2
tunaabop <tinawang158@gmail.com>
2023-04-06 01:25:25 -0400

    bcrypt fix

--- models/post.js
+++ models/post.js
@@ -1,5 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
 const sequelize = require('../config/connection');
 const { format } = require('prettier');
 
